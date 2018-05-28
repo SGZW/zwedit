7ca443bdbff87a2a8ba775b6ff5952edbd280f27
SGZW <279892296@qq.com>
2018-05-28 21:32:50 +0800

    cancel fe vue-i18n

--- zwedit_fe/src/pages/demo/index.js
+++ zwedit_fe/src/pages/demo/index.js
@@ -1,45 +1,17 @@
 import Vue from 'vue';
 import iView from 'iview';
 import VueRouter from 'vue-router';
-import VueI18n from 'vue-i18n';
 import Util from '@/libs/util';
 import router from '@/router/demo';
 import App from './App.vue';
 import '@/themes/index.less';
 
-import zhLocale from 'iview/dist/locale/zh-CN';
-import enLocale from 'iview/dist/locale/en-US';
-import jaLocale from 'iview/dist/locale/ja-JP';
-import ptLocale from 'iview/dist/locale/pt-BR';
-
 Vue.config.productionTip = false;
 Vue.config.debug = true;
 
 Vue.use(VueRouter);
-Vue.use(VueI18n);
 
-const messages = {
-    'zh-CN': zhLocale,
-    'en-US': enLocale,
-    'ja-JP': jaLocale,
-    'pt-BR': ptLocale,
-};
-
-const i18n = new VueI18n({
-    locale: Util.languageCode(),
-    fallbackLocale: 'zh-CN',
-    messages,
-});
-
-Vue.use(iView, {
-    i18n: function (path, options) {
-        let value = i18n.t(path, options);
-        if (value !== null && value !== undefined) {
-            return value;
-        }
-        return '';
-    },
-});
+Vue.use(iView);
 
 router.beforeEach((to, from, next) => {
     iView.LoadingBar.start();
@@ -55,7 +27,6 @@ router.afterEach((to, from, next) => {
 new Vue({
     el: '#app',
     router,
-    i18n,
     template: '<App/>',
     components: { App },
 });
